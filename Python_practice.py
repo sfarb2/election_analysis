96a9bac93e5e6bf7fd220f1bd94b38b6bad7a7e0
Sam Farber <sfarber@obtainmedrecs.com>
2022-08-24 17:08:30 -0400

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,16 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
